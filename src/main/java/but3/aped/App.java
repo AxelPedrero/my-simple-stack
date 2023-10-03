0464f029a16f4101dced3b0cdbd633c8214a2b77
axel.pedrero <axel.pedrero@iut-rodez.fr>
2023-10-03 17:32:28 +0200

    Debut du projet

---
+++ src/main/java/but3/aped/App.java
@@ -0,0 +1,14 @@
+package but3.aped;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+    }
+}
